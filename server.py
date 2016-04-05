7e3b2cb940409851293c38798973056f6bb53629
trambelus <trambelus2@gmail.com>
2016-04-05 16:08:17 -0400

    Completed demo using Highcharts

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import sqlite3
 import time
 import shelve
 
+import testgen
+
 DB_NAME = 'ustat.db3'
 
 app = Flask(__name__)
@@ -42,42 +44,42 @@ def get_csv(period='total'):
 			100*CAST(f.N AS FLOAT)/T fri,
 			100*CAST(g.N AS FLOAT)/T sat
 			FROM (
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
 				    GROUP BY hour) z
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '0' GROUP BY hour) a
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '1' GROUP BY hour) a
 				ON (z.hour = a.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '1' GROUP BY hour) b
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '2' GROUP BY hour) b
 				ON (z.hour = b.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '2' GROUP BY hour) c
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '3' GROUP BY hour) c
 				ON (z.hour = c.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '3' GROUP BY hour) d
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '4' GROUP BY hour) d
 				ON (z.hour = d.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '4' GROUP BY hour) e
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '5' GROUP BY hour) e
 				ON (z.hour = e.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '5' GROUP BY hour) f
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '6' GROUP BY hour) f
 				ON (z.hour = f.hour)
 			  	LEFT JOIN
-				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM comments
-				    AND STRFTIME('%w',timestamp) = '6' GROUP BY hour) g
+				  (SELECT STRFTIME('%H',timestamp) hour, SUM(pixels) N FROM stats
+				    AND STRFTIME('%w',timestamp) = '0' GROUP BY hour) g
 				ON (z.hour = g.hour)
 			  	LEFT JOIN
-				  (SELECT COUNT(*) T FROM comments)
+				  (SELECT COUNT(*) T FROM stats)
 			)
 			""").fetchall()
 	elif period == 'week':
-
+		pass
 
 	csv = '\n'.join([','.join(map(str,c)) for c in res])
 	csv = 'date,pixels\n' + csv
@@ -123,14 +125,22 @@ def rooms():
 
 		flash('Authentication successful')
 
-	csv_total = get_csv()
+	#csv_total = get_csv()
 	# with open('data.csv','r') as f:
 	# 	csvdata = f.read()
 	# csvdata = csvdata.replace('\n','\\n')
-	return render_template('index.html', csv_total=csv_total)
+	#return render_template('index.html', csv_total=csv_total)
+	return testgen.render()
 
 def main():
-	app.run(host='0.0.0.0', port=80, debug=True)
+	try:
+		app.run(host='0.0.0.0', port=80, debug=True)
+	except OSError:
+		app.run(host='0.0.0.0', port=9861, debug=True)
 
 if __name__ == '__main__':
 	main()
+
+# Pareto distribution for test data generation:
+# [[i,j,sum([par((abs(i-13+random.randint(-1,3))+0.5)/2)-((j==0 or j==6)*5)>1.5
+#  for _ in range(120)])] for i in range(24) for j in range(7)]
