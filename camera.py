a613c1d3b01ce41449affe6ed5878a266a3b7077
ccox04 <ccox04@vt.edu>
2016-04-13 16:32:53 -0400

    Corrected timer values for pictures

--- camera.py
+++ camera.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# ustat
+# Last Edit: 4/13/2016; Chris Cox
+# Edit Comments: 
+#	1) Added global variable to change the time in between pictures
+#	2) 
+
 from PIL import Image, ImageFilter, ImageChops
 import picamera
 import math, operator
@@ -6,59 +12,72 @@ import time
 import requests
 camera = picamera.PiCamera()
 
+DELAY_RUN = 60 # 60 second delay in between pictures
+DELAY_CALIBRATION = 10 # 10 second delay in between pictures
+
 def main():
 	try:
-		print('Inside of main')
-		#testing
-		#put all in a while loop, always wait for a signal to take another photo and process again
+		# put all in a while loop, always wait for a signal to take another photo and process again
 		watchDog_t = 0 # This is for the watchdog timer; Calibration Mode
 		while (1):
-			#recv_data = 5
-			print('Infinite while')
-			# recv_data = ord(conn.recv(1))
-			# print (recv_data)
-			time.sleep(10) #for demo -- protoype should be per minute
-			print ("Ten Seconds")
-			camera.capture('dump.jpg')
-			time.sleep(3)
-			camera.capture('orig.jpg') 
-			time.sleep(3)
-			camera.capture('update.jpg') #capture new image whenever there is a change
+			if watchDog_t < 61: # If FALSE; Stops posting the calibration picture after 10 minutes
+				# ***Calibration Mode***
+				time.sleep(DELAY_CALIBRATION) # Delay to take new pictures
+				print ("Ten Seconds")
+				camera.capture('dump.jpg')
+				time.sleep(3)
+				camera.capture('orig.jpg') 
+				time.sleep(3)
+				camera.capture('update.jpg') # capture new image whenever there is a change
 
-			img1 = Image.open('orig.jpg')
-			img2 = Image.open('update.jpg')
+				img1 = Image.open('orig.jpg')
+				img2 = Image.open('update.jpg')
+				print ("Captured Images")
 
-			print ("Captured Images")
+				img1 = img1.filter(ImageFilter.FIND_EDGES)
+				img1.save('orig.jpg')
+				img2 = img2.filter(ImageFilter.FIND_EDGES)
+				img2.save('update.jpg')
 
-			# toSend = img2.resize((400, 400), Image.ANTIALIAS)
-			# toSend.save('latest.png')
+				headers = {'Auth':'8spWsLd38ji08Tpc'}
 
-			img1 = img1.filter(ImageFilter.FIND_EDGES)
-			img1.save('orig.jpg')
-			img2 = img2.filter(ImageFilter.FIND_EDGES)
-			img2.save('update.jpg')
-
-			diff = ImageChops.subtract(img2, img1)
-			diff.save('diff.jpg')
-
-			diff = Image.open("diff.jpg").convert('1')
-			black, white = diff.getcolors()
-
-			print (black[0]) #number of black pixels
-			print (white[0]) #number of white pixels
-
-			headers = {'Auth':'8spWsLd38ji08Tpc'}
-			myData = {'pixels': white[0], 'roomid':0}
-
-			# ***Calibration Mode***
-			toSend = img2.resize((400, 400), Image.ANTIALIAS)
-			toSend.save('latest.png')
-			calPicture = {'calibration': open('latest.png','rb')}
-			if watchDog_t < 61: # If true this stops posting the calibration picture after 10 minutes
+				toSend = img2.resize((400, 400), Image.ANTIALIAS)
+				toSend.save('latest.png')
+				calPicture = {'calibration': open('latest.png','rb')}
 				rsp = requests.post('http://trambel.us/ustat/calibrate', files=calPicture, headers=headers) # calibration mode
 			else:
+				time.sleep(DELAY_RUN) # Delay to take new pictures
+				print ("Sixty Seconds")
+				camera.capture('dump.jpg')
+				time.sleep(3)
+				camera.capture('orig.jpg') 
+				time.sleep(3)
+				camera.capture('update.jpg') # capture new image whenever there is a change
+
+				img1 = Image.open('orig.jpg')
+				img2 = Image.open('update.jpg')
+				print ("Captured Images")
+
+				img1 = img1.filter(ImageFilter.FIND_EDGES)
+				img1.save('orig.jpg')
+				img2 = img2.filter(ImageFilter.FIND_EDGES)
+				img2.save('update.jpg')
+
+				diff = ImageChops.subtract(img2, img1)
+				diff.save('diff.jpg')
+
+				diff = Image.open("diff.jpg").convert('1')
+				black, white = diff.getcolors()
+
+				print (black[0]) #number of black pixels
+				print (white[0]) #number of white pixels
+
+				headers = {'Auth':'8spWsLd38ji08Tpc'}
+				myData = {'pixels': white[0], 'roomid':0}
+
 				rsp = requests.post('http://trambel.us/ustat/upload', data=myData, headers=headers) # graph data
-			watchDog_t += 1
+
+			watchDog_t += 1 # Increasing the watchdog timer value
 	except KeyboardInterrupt:
 		print ("keyboard interrupt")
 
