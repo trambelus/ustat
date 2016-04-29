0eb8aa2c2e91fb66653e2a78332518a8bba25e75
trambelus <trambelus2@gmail.com>
2016-04-29 19:01:25 -0400

    Briefly didn't comprehend list comprehensions

--- server.py
+++ server.py
@@ -82,7 +82,7 @@ def get_csv_heatmap():
 	""").fetchall()
 
 	#print(res)
-	mm = max([max([rr if rr != None for rr in r]) for r in res])/100
+	mm = max([max([rr for rr in r if rr != None]) for r in res])/100
 	csv_heatmap = [[i[0], j, i[j+1]/mm] for i in res for j in range(len(i)-1)]
 	return csv_heatmap
 
