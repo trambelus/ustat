efb79cc27b8adcdb09c9642cdc116d7dd85a3468
ccox04 <ccox04@vt.edu>
2016-05-06 19:34:42 -0400

    took out prints for log

--- camera.py
+++ camera.py
@@ -33,7 +33,6 @@ def thresholdCalc(AVERAGE_L):
 	if len(AVERAGE_L) < 11:
 		AVERAGE_L.append(white[0])
 		log("Appended value to initial list : ", white[0])
-		print("Appended value to initial list : ", white[0])
 		return white[0]
 	else: # This section will end up occuring everytime once list is full
 		print("List size : ", len(AVERAGE_L))
@@ -45,14 +44,12 @@ def thresholdCalc(AVERAGE_L):
 		print("total_L = ", TOTAL_L)
 		DYNAMIC_THRESHOLD_VAL = TOTAL_L / len(AVERAGE_L)
 		log("dynamic_threshold_val : ", DYNAMIC_THRESHOLD_VAL)
-		print("dynamic_threshold_val : ", DYNAMIC_THRESHOLD_VAL)
 		return DYNAMIC_THRESHOLD_VAL
 
 # This handles the calibration image for the user which can be viewed @ trambel.us/ustat/calibrate
 def calibrationMode():
 	time.sleep(DELAY_CALIBRATION) # Delay to take new pictures
 	log("Waited Ten Seconds For New Calibration")
-	print ("Waited Ten Seconds For New Calibration")
 
 	camera.capture('dump.jpg')
 	time.sleep(3)
@@ -62,7 +59,6 @@ def calibrationMode():
 	img1 = Image.open('orig.jpg')
 	img2 = Image.open('update.jpg')
 	log("Captured New Calibration Image")
-	print ("Captured New Calibration Image")
 
 	img1 = img1.filter(ImageFilter.FIND_EDGES)
 	img1.save('orig.jpg')
@@ -76,16 +72,13 @@ def calibrationMode():
 	try:
 		rsp = requests.post('http://trambel.us/ustat/calibrate', files=calPicture, headers=HEADERS) # calibration mode
 		log("Check New Calibration Photo")
-		print("Check New Calibration Photo")
 	except:
 		log("Connection Failed: Check Server Status")
-		print("Connection Failed: Check Server Status")
 		pass
 
 # This handles the image detection for the graph which can be viewed @ trambel.us/ustat
 def cameraRun():
 	log("Will Now Wait Ten Seconds")
-	print ("Will Now Wait Ten Seconds")
 	time.sleep(DELAY_RUN) # Delay to take new pictures
 	camera.capture('dump.jpg')
 	time.sleep(3)
@@ -95,7 +88,6 @@ def cameraRun():
 	img1 = Image.open('orig.jpg')
 	img2 = Image.open('update.jpg')
 	log("Captured Images")
-	print ("Captured Images")
 
 	img1 = img1.filter(ImageFilter.FIND_EDGES)
 	img1.save('orig.jpg')
@@ -107,22 +99,17 @@ def cameraRun():
 	black, white = diff.getcolors()
 	finalThresholdValue = thresholdCalc(white[0]) # Calling thresholdCalc()
 	log("finalThresholdValue : ", finalThresholdValue)
-	print("finalThresholdValue : ", finalThresholdValue)
 
 	log("Number of Black Pixels: ", black[0])
-	print ("Number of Black Pixels: ", black[0]) #number of black pixels
 	log("Number of White Pixels: ", white[0])
-	print ("Number of White Pixels: ", white[0]) #number of white pixels
 	myData = {'pixels': white[0], 'roomid':0}
 
 	# The following try block makes sure the server is up and running to prevent program crashes
 	try:
 		rsp = requests.post('http://trambel.us/ustat/upload', data=myData, headers=HEADERS) # graph data
 		log("Posted New Pixel Count To Server")
-		print("Posted New Pixel Count To Server")
 	except:
 		log("Connection Failed: Check Server Status")
-		print("Connection Failed: Check Server Status")
 		pass
 
 def main():
@@ -130,7 +117,6 @@ def main():
 		# put all in a while loop, always wait for a signal to take another photo and process again
 		watchDog_t = 0 # This is for the watchdog timer; Calibration Mode
 		log("Preparing Camera...")
-		print("Preparing Camera...")
 		while (1):
 			if watchDog_t < 8: # If FALSE; Stops posting the calibration picture after 2 minutes
 				# ***Calibration Mode***
