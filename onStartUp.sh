442da2698a5f572027d839a178c28dc9c7c79bce
ccox04 <ccox04@vt.edu>
2016-05-06 19:16:09 -0400

    trying something new

--- onStartUp.sh
+++ onStartUp.sh
@@ -4,6 +4,10 @@
 # Date: 5/6/2016
 # Runs camera.py on start up by changing directories and then back to original dirSectory
 
+# Sends this device's local wlan0 IP address to the PiSSH server, and clears it on shutdown.
+ipaddr=$(ifconfig eth0 | awk '/inet addr/{{print substr($2,6)}}')
+echo "PiSSH: Sending IP $ipaddr to server"
+
 cd /
 cd home/pi/ustat
 sudo python3 camera.py
